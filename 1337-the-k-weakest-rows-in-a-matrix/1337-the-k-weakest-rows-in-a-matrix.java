cf4ec93f0f7e35ee2f8c1d5986fff123bfde3fe6
notsogoodaditya <79082526+notsogoodaditya@users.noreply.github.com>
2022-08-11 01:46:29 +0530

    Time: 16 ms (5.41%), Space: 50.1 MB (5.52%) - LeetHub

---
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class Solution {
+    public int[] kWeakestRows(int[][] mat, int k) {
+        HashMap<Integer,ArrayList<Integer>> map = new HashMap<>();
+        for(int i= 0;i<mat.length;i++){
+            int one = 0;
+            for(int j=0;j<mat[0].length;j++){
+                one += mat[i][j];
+            }
+            if(map.get(one)!=null){
+                ArrayList<Integer> arr = map.get(one);
+                arr.add(i);
+                map.put(one,arr);
+            }else{
+                ArrayList<Integer> arr = new ArrayList<>();
+                arr.add(i);
+                map.put(one,arr);
+            }
+        }int ans[] = new int[k];
+        int pos = 0;
+         ArrayList<Integer> set = new ArrayList<>(map.keySet());
+        Collections.sort(set);
+        for(int key : set){
+            for(int i : map.get(key)){
+                System.out.print(i+" ");
+                ans[pos] = i;
+                pos++;
+                k--;
+                if(k==0 || pos==ans.length){
+                    break;
+                }
+            }
+            if(k==0 || pos==ans.length){
+                break;
+            }
+        }
+        System.out.println();
+        System.out.println(map);
+        return ans;
+    }
+}
