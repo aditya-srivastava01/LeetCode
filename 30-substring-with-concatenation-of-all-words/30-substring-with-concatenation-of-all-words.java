d3a5ce8c91a67cf5960c02c597862eaadb305193
notsogoodaditya <79082526+notsogoodaditya@users.noreply.github.com>
2022-08-13 19:44:52 +0530

    Time: 1972 ms (5.02%), Space: 119.2 MB (10.24%) - LeetHub

---
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.java
@@ -0,0 +1,49 @@
+import java.util.*;
+class Solution {
+    Hashtable<String,Integer> map = new Hashtable<>();
+    public List<Integer> findSubstring(String str, String[] words) {
+        List<Integer> ans = new ArrayList<>();
+        StringBuilder s = new StringBuilder(str);
+        int len = words.length*words[0].length();
+        int n = s.length();
+        if(len>n){
+            return ans;
+        }
+        StringBuilder slide = new StringBuilder(s.substring(0,len));
+        for(String word:words){
+            map.putIfAbsent(word,0);
+            map.put(word,map.get(word)+1);
+        }
+        if(check(String.valueOf(slide),words[0].length())){
+            ans.add(0);
+        }
+        for(int i=len;i<n;i++){
+            slide.deleteCharAt(0);
+            slide.append(s.charAt(i));
+            if(check(String.valueOf(slide),words[0].length())){
+                ans.add(i-len+1);
+            }
+        }
+        return ans;
+    }
+    public boolean check(String word,int len){
+        String[] chunks = word.split("(?<=\\G.{" + len + "})");
+        Hashtable<String,Integer> count = new Hashtable<>();
+        for(String s : chunks){
+            count.putIfAbsent(s,0);
+            count.put(s,count.get(s)+1);
+            if(map.get(s)==null){
+                return false;
+            }
+            if(count.get(s)>map.get(s)){
+                return false;
+            }
+        }
+        for(String s: chunks){
+            if(!map.get(s).equals(count.get(s))){
+                return false;
+            }
+        }
+        return true;
+    }
+}
