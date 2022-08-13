cfd1f96ad6875a8877e8e095ef52ec3d1075b4f5
notsogoodaditya <79082526+notsogoodaditya@users.noreply.github.com>
2022-08-13 11:21:22 +0530

    Time: 4 ms (47.07%), Space: 68.1 MB (44.67%) - LeetHub

---
+++ 55-jump-game/55-jump-game.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int max = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                max = Math.max(nums[i]+i,max);
+            }else{
+                if(max>=nums.length-1){
+                    return true;
+                }
+                if(max>i){
+                    for(int j=i;j<max;j++){
+                        // System.out.print(nums[j]+ " ");
+                        if(nums[j]!=0){
+                            i = j;
+                            max = Math.max(nums[i]+i,max);
+                            break;
+                        }
+                    }
+                    // System.out.println();
+                }else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
