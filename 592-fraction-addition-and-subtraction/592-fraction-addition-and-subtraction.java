2ef01f2045e6c0d148eea07b0a1fcfd3a75b2c9a
notsogoodaditya <79082526+notsogoodaditya@users.noreply.github.com>
2022-08-18 12:16:11 +0530

    Time: 14 ms (46.67%), Space: 42.9 MB (49.33%) - LeetHub

--- 592-fraction-addition-and-subtraction/592-fraction-addition-and-subtraction.java
+++ 592-fraction-addition-and-subtraction/592-fraction-addition-and-subtraction.java
@@ -3,7 +3,6 @@ class Solution {
         List<Integer> num = new ArrayList<>();
         List<Integer> denom = new ArrayList<>();
         List<String> ope_ope_no_mi = new ArrayList<>();
-        // char[] s = expression.toCharArray();
         int n = expression.length();
         if(expression.charAt(0)!='-'){
             ope_ope_no_mi.add("+");
@@ -24,10 +23,10 @@ class Solution {
             }
         }
         n = num.size();
-        System.out.println(num);
-        System.out.println(denom);
-        System.out.println(ope_ope_no_mi);
-        System.out.print(lcm(denom, 0)+ "\n");
+        // System.out.println(num);
+        // System.out.println(denom);
+        // System.out.println(ope_ope_no_mi);
+        // System.out.print(lcm(denom, 0)+ "\n");
         int denominator = lcm(denom,0);
         int numerator = 0;
         for(int i=0;i<n;i++){
@@ -37,10 +36,12 @@ class Solution {
                 numerator -= num.get(i)*(denominator/denom.get(i));
             }
         }
+        
         int div = gcd(numerator,denominator);
         numerator /= div;
         denominator /= div;
         String sign = "";
+        
         if(numerator<0 && denominator>0){
              numerator *=-1;
             sign = "-";
