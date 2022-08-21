01ed1a6b305398da49bfde98eb22c29cdbc17c1e
notsogoodaditya <79082526+notsogoodaditya@users.noreply.github.com>
2022-08-21 19:16:25 +0530

    Time: 1 ms (87.37%), Space: 44.2 MB (18.87%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
@@ -1,7 +1,18 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        Arrays.sort(nums);
-        return binarySearch(nums,0,nums.length-1,target);
+        // Arrays.sort(nums);
+        int n = nums.length;
+        int pos = n-1;
+        for(int i=0;i<n-1;i++){
+            if(nums[i]==target || nums[i+1]==target){
+                return true;
+            }
+            if(nums[i+1]<nums[i]){
+                pos = i;
+                break;
+            }
+        }
+        return binarySearch(nums,0,pos,target)||binarySearch(nums,pos+1,n-1,target);
     }
     public boolean binarySearch(int arr[], int l, int r, int x){
         if (r >= l) {
