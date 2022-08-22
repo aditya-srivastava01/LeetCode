17c621cd68e7966d96c9cbf230d65f190d5077da
notsogoodaditya <79082526+notsogoodaditya@users.noreply.github.com>
2022-08-22 10:18:54 +0530

    Time: 2 ms (65.34%), Space: 41.8 MB (19.85%) - LeetHub

--- 342-power-of-four/342-power-of-four.java
+++ 342-power-of-four/342-power-of-four.java
@@ -1,10 +1,18 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
         int[] four  = {1,4,16,64,256,1024,4096,16384,65536,262144,1048576,4194304,16777216,67108864,268435456,1073741824};
-        for(int i=0;i<16;i++){
-            if(four[i]==n){
+        return search(four,0,four.length-1,n);
+    }
+    public boolean search(int[] four,int l ,int r,int x){
+        if(r>=l){
+            int mid = l+(r-l)/2;
+            if(four[mid]==x){
                 return true;
             }
+            if(four[mid]>x){
+                return search(four,l,mid-1,x);
+            }
+            return search(four,mid+1,r,x);
         }
         return false;
     }
