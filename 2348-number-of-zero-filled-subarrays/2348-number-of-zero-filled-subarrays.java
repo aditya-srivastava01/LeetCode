184bf2c54fa7d17c7097d27608ab69a816bc8a06
notsogoodaditya <79082526+notsogoodaditya@users.noreply.github.com>
2022-08-23 11:17:36 +0530

    Time: 16 ms (6.81%), Space: 97.6 MB (46.39%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.java
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.java
@@ -1,9 +1,11 @@
+import java.util.*;
 class Solution {
     public long zeroFilledSubarray(int[] nums) {
         long ans = 0;
         int i = 0;
         int j = 0;
         int n = nums.length;
+        Hashtable<Long,Long> map = new Hashtable<>();
         for(i=0;i<n;i+=0){
             if(nums[i]==0){
                 for(j=i;j<n;j++){
@@ -12,8 +14,16 @@ class Solution {
                     }
                 }
                 long size = j-i;
-                long sum = ((size+1)*(size+2))/2;
-                ans += size*(size+1)+(size+1)-sum;
+                if(map.get(size)!=null){
+                    ans += map.get(size);
+                }else{
+                    long sum = ((size+1)*(size+2))/2;
+                    long val = size*(size+1)+(size+1)-sum;
+                    ans += size*(size+1)+(size+1)-sum;
+                    map.put(size,val);
+                }
+                // long sum = ((size+1)*(size+2))/2;
+                // ans += size*(size+1)+(size+1)-sum;
                 i += size;
             }else{
                 i++;
