2ebd7a6f2999a36cb30f9a0361831537d1d327ce
notsogoodaditya <79082526+notsogoodaditya@users.noreply.github.com>
2022-08-24 23:21:07 +0530

    Time: 12 ms (33.21%), Space: 45.8 MB (10.67%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.java
+++ 692-top-k-frequent-words/692-top-k-frequent-words.java
@@ -7,23 +7,22 @@ class Solution {
         for(int i=0;i<n;i++){
             map.put(words[i],map.getOrDefault(words[i],0)+1);
         }
-        Hashtable<Integer,List<String>> val = new Hashtable<>();
+        Hashtable<Integer,PriorityQueue<String>> val = new Hashtable<>();
         for(String word : map.keySet()){
-            List<String> list = new ArrayList();
+            PriorityQueue<String> list = new PriorityQueue<String>();
             if(val.get(map.get(word))!=null){
                 list = val.get(map.get(word));
             }
             list.add(word);
             val.put(map.get(word),list);
         }
-        System.out.println(val);
+        // System.out.println(val);
         List<Integer> count = new ArrayList<>(val.keySet());
         Collections.sort(count);
         for(int i=count.size()-1;i>=0;i--){
-            List<String> list = val.get(count.get(i));
-            Collections.sort(list);
-            for(String word:list){
-                ans.add(word);
+            PriorityQueue<String> list = val.get(count.get(i));
+            while(!list.isEmpty()){
+                ans.add(list.poll());
                 k--;
                 if(k==0){
                     break;
