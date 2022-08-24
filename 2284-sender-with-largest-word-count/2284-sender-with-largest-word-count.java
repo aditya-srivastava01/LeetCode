31ca6b0e5a0969911a839e207deeb3fb0bfe1c30
notsogoodaditya <79082526+notsogoodaditya@users.noreply.github.com>
2022-08-24 23:48:43 +0530

    Time: 214 ms (13.41%), Space: 111.6 MB (15.20%) - LeetHub

---
+++ 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Solution {
+    public String largestWordCount(String[] messages, String[] senders) {
+        int n = senders.length;
+        int max = 0;
+        Hashtable<String,Integer> map = new Hashtable<>();
+        for(int i=0;i<n;i++){
+            String[] s = messages[i].split(" ");
+            int size = s.length;
+            map.put(senders[i],map.getOrDefault(senders[i],0)+size);
+            max = Math.max(max,map.get(senders[i]));
+        }
+        System.out.println(map);
+        PriorityQueue<String> ans = new PriorityQueue<>((a,b)->b.compareTo(a));
+        for(String word : map.keySet()){
+            if(map.get(word)==max){
+                ans.add(word);
+            }
+        }
+        return ans.peek();
+    }
+    public int hash(String word){
+        int p = 0;
+        for (int i=0;i<word.length();i++){
+            p = (128*p + word.charAt(i))%83;
+        }
+        return p;
+    }
+}
