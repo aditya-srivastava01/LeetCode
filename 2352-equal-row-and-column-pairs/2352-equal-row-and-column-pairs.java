616d5a5db817baf8699c4ab0bfb4363a445b412c
notsogoodaditya <79082526+notsogoodaditya@users.noreply.github.com>
2022-08-27 21:36:28 +0530

    Time: 156 ms (21.55%), Space: 119.1 MB (6.96%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
@@ -3,8 +3,6 @@ class Solution {
     public int equalPairs(int[][] grid) {
         int n = grid.length;
         Hashtable<String,Integer> row = new Hashtable<>();
-        Hashtable<String,Integer> col = new Hashtable<>();
-        Set<String> set = new HashSet<>();
         for(int[] r:grid){
             String[] num = new String[n];
             for(int i=0;i<n;i++){
@@ -12,23 +10,16 @@ class Solution {
             }
             String number = String.join("",num);
             row.put(number,row.getOrDefault(number,0)+1);
-            set.add(number);
         }
+        int ans = 0;
         for(int j=0;j<n;j++){
             String[] num = new String[n];
             for(int i=0;i<n;i++){
                 num[i] = "."+String.valueOf(grid[i][j]);
             }
             String number = String.join("",num);
-            col.put(number,col.getOrDefault(number,0)+1);
-            set.add(number);
-        }
-        // System.out.println(row);
-        // System.out.println(col);
-        int ans = 0;
-        for(String s : set){
-            if(row.get(s)!=null && col.get(s)!=null){
-                ans += row.get(s)*col.get(s);
+            if(row.get(number)!=null){
+                ans += row.get(number);
             }
         }
         return ans;
