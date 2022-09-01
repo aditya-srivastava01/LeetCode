aa3a889eaad5cf5fd67532b481bf16099136cc8a
notsogoodaditya <79082526+notsogoodaditya@users.noreply.github.com>
2022-09-01 09:35:10 +0530

    Time: 8 ms (73.40%), Space: 43.6 MB (60.61%) - LeetHub

---
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String reverseWords(String s) {
+        String[] str = s.split(" ");
+        int n = str.length;
+        List<String> list = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(str[i].length()>0){    
+                list.add(str[i]);
+            }    
+        }
+        Collections.reverse(list);
+        return String.join(" ",list);
+    }
+}
