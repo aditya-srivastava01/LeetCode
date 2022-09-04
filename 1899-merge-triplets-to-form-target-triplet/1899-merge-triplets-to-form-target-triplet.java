08469872417f15f92c22aa6d2c93200a2c1c859d
notsogoodaditya <79082526+notsogoodaditya@users.noreply.github.com>
2022-09-04 22:25:05 +0530

    Time: 4 ms (81.11%), Space: 133.3 MB (83.29%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.java
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.java
@@ -1,11 +1,17 @@
 class Solution {
-    public boolean mergeTriplets(int[][] trip, int[] target) {
+    public boolean mergeTriplets(int[][] tri, int[] target) {
         int[] ans = {0,0,0};
-        for(int[] tri:trip){
-            if(tri[0]<=target[0] && tri[1]<=target[1] && tri[2]<=target[2]){
-                ans[0] = Math.max(ans[0],tri[0]);
-                ans[1] = Math.max(ans[1],tri[1]);
-                ans[2] = Math.max(ans[2],tri[2]);
+        int n = tri.length;
+        for(int i=0;i<=n/2;i++){
+            if(tri[i][0]<=target[0] && tri[i][1]<=target[1] && tri[i][2]<=target[2]){
+                ans[0] = Math.max(ans[0],tri[i][0]);
+                ans[1] = Math.max(ans[1],tri[i][1]);
+                ans[2] = Math.max(ans[2],tri[i][2]);
+            }
+            if(tri[n-i-1][0]<=target[0] && tri[n-i-1][1]<=target[1] && tri[n-i-1][2]<=target[2]){
+                ans[0] = Math.max(ans[0],tri[n-i-1][0]);
+                ans[1] = Math.max(ans[1],tri[n-i-1][1]);
+                ans[2] = Math.max(ans[2],tri[n-i-1][2]);
             }
             if(ans[0]==target[0] && ans[1]==target[1] && ans[2]==target[2]){
                 return true;
