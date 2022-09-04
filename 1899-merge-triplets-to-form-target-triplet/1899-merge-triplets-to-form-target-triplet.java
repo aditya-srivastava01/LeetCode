24dd50b78d2cfb17c6d50919a42849039bda8423
notsogoodaditya <79082526+notsogoodaditya@users.noreply.github.com>
2022-09-04 22:28:19 +0530

    Time: 3 ms (84.32%), Space: 133.8 MB (77.51%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.java
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.java
@@ -3,15 +3,21 @@ class Solution {
         int[] ans = {0,0,0};
         int n = tri.length;
         for(int i=0;i<=n/2;i++){
-            if(tri[i][0]<=target[0] && tri[i][1]<=target[1] && tri[i][2]<=target[2]){
-                ans[0] = Math.max(ans[0],tri[i][0]);
-                ans[1] = Math.max(ans[1],tri[i][1]);
-                ans[2] = Math.max(ans[2],tri[i][2]);
+            int x = tri[i][0];
+            int y = tri[i][1];
+            int z = tri[i][2];
+            if(x<=target[0] && y<=target[1] && z<=target[2]){
+                ans[0] = Math.max(ans[0],x);
+                ans[1] = Math.max(ans[1],y);
+                ans[2] = Math.max(ans[2],z);
             }
-            if(tri[n-i-1][0]<=target[0] && tri[n-i-1][1]<=target[1] && tri[n-i-1][2]<=target[2]){
-                ans[0] = Math.max(ans[0],tri[n-i-1][0]);
-                ans[1] = Math.max(ans[1],tri[n-i-1][1]);
-                ans[2] = Math.max(ans[2],tri[n-i-1][2]);
+            x = tri[n-i-1][0];
+            y = tri[n-i-1][1];
+            z = tri[n-i-1][2];
+            if(x<=target[0] && y<=target[1] && z<=target[2]){
+                ans[0] = Math.max(ans[0],x);
+                ans[1] = Math.max(ans[1],y);
+                ans[2] = Math.max(ans[2],z);
             }
             if(ans[0]==target[0] && ans[1]==target[1] && ans[2]==target[2]){
                 return true;
