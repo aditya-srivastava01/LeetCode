6b7a6d785945994b3d3310cd0b35a5a2a41ce282
notsogoodaditya <79082526+notsogoodaditya@users.noreply.github.com>
2022-09-04 16:33:13 +0530

    Time: 31 ms (16.91%), Space: 42.2 MB (19.12%) - LeetHub

--- 1362-closest-divisors/1362-closest-divisors.java
+++ 1362-closest-divisors/1362-closest-divisors.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] closestDivisors(int num) {
         int ans[] = new int[2];
         int min = Integer.MAX_VALUE;
-        for(int i=1;i<=(int)Math.sqrt(num+1)+1;i++){
+        for(int i=1;i<=(int)Math.sqrt(num+2)+1;i++){
             if((num+1)%i==0){
                 int val = Math.abs(i-(num+1)/i);
                 if(val<min){
@@ -10,25 +10,19 @@ class Solution {
                     ans[0] = i;
                     ans[1] = (num+1)/i;
                 }
-                if(min==0){
-                    return ans;
-                }
             }
-        }
-        for(int i=1;i<=(int)Math.sqrt(num+2)+1;i++){
-             if((num+2)%i==0){
+            if((num+2)%i==0){
                 int val = Math.abs(i-(num+2)/i);
                 if(val<min){
                     min = val;
                     ans[0] = i;
                     ans[1] = (num+2)/i;
                 }
-                 if(min==0){
-                     return ans;
-                 }
-            }           
+            }
+            if(min==0){
+                return ans;
+            }
         }
-        
         return ans;
     }
 }
