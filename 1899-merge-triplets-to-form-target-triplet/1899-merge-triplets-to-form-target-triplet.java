2ab458258ae94dc1a1d6478f8bfc61a39793f5a6
notsogoodaditya <79082526+notsogoodaditya@users.noreply.github.com>
2022-09-04 22:19:22 +0530

    Time: 2 ms (92.67%), Space: 166.4 MB (29.05%) - LeetHub

---
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean mergeTriplets(int[][] trip, int[] target) {
+        int[] ans = {0,0,0};
+        for(int[] tri:trip){
+            if(tri[0]<=target[0] && tri[1]<=target[1] && tri[2]<=target[2]){
+                ans[0] = Math.max(ans[0],tri[0]);
+                ans[1] = Math.max(ans[1],tri[1]);
+                ans[2] = Math.max(ans[2],tri[2]);
+            }
+            if(ans[0]==target[0] && ans[1]==target[1] && ans[2]==target[2]){
+                return true;
+            }
+        }
+        // for(int i:ans){
+        //     System.out.print(i+" ");
+        // }
+        // System.out.println();
+        if(ans[0]==target[0] && ans[1]==target[1] && ans[2]==target[2]){
+            return true;
+        }
+        return false;
+    }
+}
