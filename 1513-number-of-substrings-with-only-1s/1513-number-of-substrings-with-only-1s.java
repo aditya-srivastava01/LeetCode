c623b93c760a6d9ac0798e16307da49ce1b3fa8a
notsogoodaditya <79082526+notsogoodaditya@users.noreply.github.com>
2022-09-05 17:49:27 +0530

    Time: 30 ms (5.24%), Space: 49.3 MB (6.54%) - LeetHub

--- 1513-number-of-substrings-with-only-1s/1513-number-of-substrings-with-only-1s.java
+++ 1513-number-of-substrings-with-only-1s/1513-number-of-substrings-with-only-1s.java
@@ -1,13 +1,20 @@
 import java.util.*;
 class Solution {
-    Hashtable<String,Integer> map = new Hashtable<>();
+    Hashtable<String,Long> map = new Hashtable<>();
     public int numSub(String str) {
         // System.out.println(s.substring(0,2));
         String[] s = str.split("0");
         long ans = 0;
         for(int i=0;i<s.length;i++){
             // System.out.println(s[i]);
-            ans += ((long)s[i].length()*((long)s[i].length()+1l))/2l;
+            if(map.get(s[i])==null){
+                long cnt = ((long)s[i].length()*((long)s[i].length()+1l))/2l;
+                map.put(s[i],cnt);
+                ans += cnt;
+            }else{
+                ans += map.get(s[i]);
+            }
+            
         }
         
         return (int)(ans%1000000007l);
