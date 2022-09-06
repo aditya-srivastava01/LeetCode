ff48b6c47094cb3e69d0f08ef463bf978a46dda4
notsogoodaditya <79082526+notsogoodaditya@users.noreply.github.com>
2022-09-06 14:45:06 +0530

    Time: 215 ms (5.13%), Space: 93.6 MB (5.05%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+    int[] arr;
+    Hashtable<List<Integer>,Boolean> map = new Hashtable<>();
+    public List<List<Integer>> combinationSum(int[] arr, int target) {
+        this.arr = arr;
+        for(int i=0;i<arr.length;i++){
+            List<Integer> list = new ArrayList<>();
+            find(i,target,list);
+        }
+        return ans;
+    }
+    public void find(int indx,int x,List<Integer> list){
+        if(indx<arr.length && x>=0){
+            if(x==0){
+                Collections.sort(list);
+                if(map.get(list)==null){
+                    ans.add(list);
+                    map.put(list,true);                    
+                }
+            }
+            List<Integer> temp = new ArrayList<>(list);
+            temp.add(arr[indx]);
+            find(indx,x-arr[indx],temp);
+            find(indx+1,x,list);          
+        }
+    }
+}
