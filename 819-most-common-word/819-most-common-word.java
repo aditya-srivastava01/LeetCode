6941d7f8cd39872e3a9cb69bc265c894c5dd0d1e
notsogoodaditya <79082526+notsogoodaditya@users.noreply.github.com>
2022-09-06 22:32:17 +0530

    Time: 14 ms (79.46%), Space: 43.9 MB (56.55%) - LeetHub

---
+++ 819-most-common-word/819-most-common-word.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Solution {
+    public String mostCommonWord(String str, String[] banned) {
+        str = str.replace("!","");
+        str = str.replace("?","");
+        str = str.replace("'","");
+        str = str.replace(","," ");
+        str = str.replace(";","");
+        str = str.replace(".","");
+        str = str.toLowerCase();
+        String[] s = str.split(" ");
+        Hashtable<String,Integer> map = new Hashtable<>();
+        for(int i=0;i<s.length;i++){
+            map.put(s[i],map.getOrDefault(s[i],0)+1);
+        }
+        for(String word : banned){
+            map.remove(word);
+        }
+        String ans = "";
+        int count = 0;
+        for(String word : map.keySet()){
+            if(word.length()>0 && map.get(word)>count){
+                count = map.get(word);
+                ans = word;
+                // System.out.println(word+" : "+map.get(word));
+            }
+        }
+        // System.out.println(map);
+        return ans;
+    }
+}
