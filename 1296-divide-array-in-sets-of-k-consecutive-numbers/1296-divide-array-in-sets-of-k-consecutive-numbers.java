fc0673b556604f0007dac2c70ac523efb8d48543
notsogoodaditya <79082526+notsogoodaditya@users.noreply.github.com>
2022-09-09 21:40:02 +0530

    Time: 1748 ms (5.14%), Space: 71.7 MB (70.73%) - LeetHub

---
+++ 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class Solution {
+    public boolean isPossibleDivide(int[] nums, int k) {
+        int n = nums.length;
+        if(k==1){
+            return  true;
+        }
+        if(n%k!=0){
+            return false;
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i=0;i<n;i++){
+            pq.add(nums[i]);
+        }
+        int prev = pq.poll();
+        int len = n/k;
+        int size = 0;
+        size++;
+        // System.out.println(prev+" : "+pq);
+        while(pq.size()>0){
+            if(pq.contains(prev+1)){
+                prev++;
+                pq.remove(prev);
+                size++;
+                // System.out.println(prev+" : "+pq);
+            }
+            
+            else{
+                // System.out.println(prev+" : "+pq);
+                return false;
+            }
+            if(size==k && pq.size()>0){
+                // System.out.println("---------------------");
+                prev = pq.poll();
+                size = 1;
+                // System.out.println(prev+" : "+pq);
+            }
+            
+        }
+        return true;
+    }
+}
