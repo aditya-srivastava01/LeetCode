fd2f1f210f557da4476c517aac030f0f3346f0d8
notsogoodaditya <79082526+notsogoodaditya@users.noreply.github.com>
2022-09-11 18:04:48 +0530

    Time: 11 ms (52.40%), Space: 55.6 MB (61.39%) - LeetHub

--- 89-gray-code/89-gray-code.java
+++ 89-gray-code/89-gray-code.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<Integer> grayCode(int n) {
         List<Integer> ans = new LinkedList<>();
-        for (int i = 0; i < 1<<n; i++){
+        for (int i = 0; i <(int) Math.pow(2,n); i++){
             ans.add(i ^ i>>1);
         }
         return ans;
