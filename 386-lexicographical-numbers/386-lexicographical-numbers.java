8b05cba7389cde59227d445993dbd27069520902
notsogoodaditya <79082526+notsogoodaditya@users.noreply.github.com>
2022-09-13 20:44:12 +0530

    Time: 41 ms (13.83%), Space: 56.2 MB (79.51%) - LeetHub

--- 386-lexicographical-numbers/386-lexicographical-numbers.java
+++ 386-lexicographical-numbers/386-lexicographical-numbers.java
@@ -2,16 +2,18 @@ import java.util.*;
 class Solution {
     public List<Integer> lexicalOrder(int n) {
         List<String> list = new ArrayList<>();
-        Hashtable<String,Integer> map = new Hashtable<>();
+        // Hashtable<String,Integer> map = new Hashtable<>();
         for(int i=1;i<=n;i++){
-            String val = String.valueOf(i);
-            map.put(val,i);
-            list.add(val);
+            // String val = String.valueOf(i);
+            // map.put(val,i);
+            // list.add(val);
+            list.add(String.valueOf(i));
         }
         Collections.sort(list);
         List<Integer> ans = new ArrayList<>();
         for(int i=0;i<n;i++){
-            ans.add(map.get(list.get(i)));
+            // ans.add(map.get(list.get(i)));
+            ans.add(Integer.valueOf(list.get(i)));
         }
         return ans;
     }
