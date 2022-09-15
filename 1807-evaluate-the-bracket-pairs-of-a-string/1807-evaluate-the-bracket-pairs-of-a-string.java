61dd35d1ecd810d9892cf156e6bf54d1106c26ab
notsogoodaditya <79082526+notsogoodaditya@users.noreply.github.com>
2022-09-16 02:53:42 +0530

    Time: 93 ms (36.75%), Space: 92.3 MB (74.78%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.java
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.java
@@ -2,27 +2,25 @@ class Solution {
     public String evaluate(String s, List<List<String>> know) {
         HashMap<String,String> map = new HashMap<>();
         for(int i=0;i<know.size();i++){
-            map.put("("+know.get(i).get(0)+")",know.get(i).get(1));
+            map.put(know.get(i).get(0),know.get(i).get(1));
         }
         char[] str = s.toCharArray();
-        // List<Character> ans = new ArrayList<>();
-        char[] ans = new char[2*1_0_0_0_0_0];
+        List<String> ans = new ArrayList<>();
+        // char[] ans = new char[10*s.length()];
         int indx = 0;
         for(int i=0;i<str.length;i++){
             if(str[i]=='('){
                 String key = "";
-                for(int j=i;j<str.length;j++){
+                for(int j=i+1;j<str.length;j++){
                     if(str[j]==')'){
-                        key += ')';
                         if(map.get(key)==null){
-                            // ans.add('?');
-                            ans[indx] = '?';
-                            indx++;
+                            ans.add("?");
+                            // ans[indx] = '?';indx++;
                         }else{
                             for(char c : map.get(key).toCharArray()){
-                                ans[indx] = c;
-                                // ans.add(c);
-                                indx++;
+                                // ans[indx] = c; indx++;
+                                ans.add(""+c);
+                               
                             }
                         }
                         i=j;
@@ -32,17 +30,11 @@ class Solution {
                 }
                 
             }else{
-                ans[indx] = str[i];
-                indx++;
-                // ans.add(str[i]);
+                // ans[indx] = str[i];indx++;
+                ans.add(""+str[i]);
             }
         }
-        // System.out.println(ans);
-        // char[] res = new char[ans.size()];
-        // for(int i=0;i<ans.size();i++){
-        //     res[i] = ans.get(i);
-        // }
-        char[] res = Arrays.copyOf(ans,indx);
-        return String.valueOf(res);
+        // char[] res = Arrays.copyOf(ans,indx);
+        return String.join("",ans);
     }
 }
