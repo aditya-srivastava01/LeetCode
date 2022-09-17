0fed9d0466f3e047087fa2919d8e47c284b66ca6
notsogoodaditya <79082526+notsogoodaditya@users.noreply.github.com>
2022-09-17 12:19:10 +0530

    Time: 54 ms (66.91%), Space: 60.3 MB (87.50%) - LeetHub

--- 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.java
+++ 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.java
@@ -1,11 +1,11 @@
 class Solution {
     public String repeatLimitedString(String str, int k) {
         int[] count = new int[26];
-        char[] s = str.toCharArray();
+        char[] ans = str.toCharArray();
         char[] alpha = "abcdefghijklmnopqrstuvwxyz".toCharArray();
-        int n = s.length;
+        int n = ans.length;
         for(int i=0;i<n;i++){
-            count[s[i]-'a']++;
+            count[ans[i]-'a']++;
         }
         PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->b[0]-a[0]);
         for(int i=0;i<26;i++){
@@ -13,58 +13,41 @@ class Solution {
                 pq.add(new int[] {i,count[i]});
             }
         }
-        char[] ans = new char[n];
         
         int[] largest = pq.poll();
         int cnt = Math.min(k,largest[1]);
         largest[1] -= cnt;
-        // char[] sub = new char[cnt];
         int indx = 0;
         Arrays.fill(ans,indx,indx+cnt,alpha[largest[0]]);  
         indx += cnt;
-        // ans += String.valueOf(sub);
-        // System.out.println(ans);
         while(!pq.isEmpty()){
             int[] second_largest = pq.poll();
             if(largest==null || largest[1]==0){
                 cnt = Math.min(k,second_largest[1]);
             }else{
                 cnt = Math.min(1,second_largest[1]);
-            }
-            
+            }   
             second_largest[1] -= cnt;
             if(second_largest[1]>0){
                 pq.add(second_largest);
             }
-            // sub = new char[cnt];
             Arrays.fill(ans,indx,indx+cnt,alpha[second_largest[0]]);
             indx += cnt;
-            // ans += String.valueOf(sub);
-            
             if(largest[1]==0 && !pq.isEmpty()){
                 largest = pq.poll();
             }
             if(largest[0]!=second_largest[0]){
                 cnt = Math.min(largest[1],k);
                 largest[1] -= cnt;
-
-                // sub = new char[cnt];
                 Arrays.fill(ans,indx,indx+cnt,alpha[largest[0]]);
-                indx+=cnt;
-                // ans += String.valueOf(sub);               
+                indx+=cnt;          
             }
-
         }
-
-        // System.out.println(largest);
         if(largest!=null && alpha[largest[0]]!=ans[indx-1]){
             cnt = Math.min(largest[1],k);
             largest[1] -= cnt;
-
-            // sub = new char[cnt];
             Arrays.fill(ans,indx,indx+cnt,alpha[largest[0]]);
-            indx += cnt;
-            // ans += String.valueOf(sub);               
+            indx += cnt;              
         }
         return String.valueOf(ans).substring(0,indx);
     }
