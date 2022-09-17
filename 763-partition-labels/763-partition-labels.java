0c6c5cec8e1cea43caa8c2a277b6eba914ce723e
notsogoodaditya <79082526+notsogoodaditya@users.noreply.github.com>
2022-09-17 13:31:59 +0530

    Time: 4 ms (91.16%), Space: 42.4 MB (79.37%) - LeetHub

--- 763-partition-labels/763-partition-labels.java
+++ 763-partition-labels/763-partition-labels.java
@@ -5,18 +5,12 @@ class Solution {
         List<Integer> ans = new ArrayList<>();
         this.s = str.toCharArray();
         int n = s.length;
-        
         for(int i=0;i<n;i++){
             map.put(s[i],i);
         }
-        // System.out.println(map);
-        for(int i=0;i<n;i++){
-            char first = s[i];
-            // Set<Character> set = new HashSet<>();
-            // set.add(first);          
+        for(int i=0;i<n;i++){      
             for(int j=n-1;j>=i;j--){
-                if(s[j]==first){
-                    // System.out.print(first+" ");
+                if(s[j]==s[i]){
                     int indx = helper(i,j);
                     while(true){
                         int val = helper(i,indx);
@@ -25,7 +19,6 @@ class Solution {
                         }
                         indx = val;
                     }
-                    // System.out.println(indx);
                     ans.add(indx-i+1);
                     i = indx;
                     break;
