d119eb827ff48da8427f78014775ce830e49a222
notsogoodaditya <79082526+notsogoodaditya@users.noreply.github.com>
2022-09-20 13:51:13 +0530

    Time: 2 ms (43.75%), Space: 41.8 MB (75.50%) - LeetHub

---
+++ 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.java
@@ -0,0 +1,29 @@
+class Solution {
+     char[] alpha = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+    public String replaceDigits(String str) {
+        char[] s = str.toCharArray();
+        int n = s.length;
+        char[] ans = new char[n];
+        int k = 0;
+        for(int i=0;i<n-1;i++){
+            if('a'<=s[i] && s[i]<='z' && '0'<=s[i+1] && s[i+1]<='9'){
+                ans[k] = s[i];
+                ans[k+1] = shift(s[i],s[i+1]);
+                k+=2;
+                i++;
+                
+            }else{
+                ans[k] = s[i];
+                k++;
+            }
+        }
+        if(!('0'<=s[n-1] && s[n-1]<='9')){
+            ans[k] = s[n-1];
+            k++;
+        }
+        return String.valueOf(ans).substring(0,k);
+    }
+    public char shift(char a ,char i){
+        return alpha[a-'a'+(i-'0')];
+    }
+}
