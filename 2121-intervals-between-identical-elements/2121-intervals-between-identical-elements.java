8fd760f8f0027f61a7ef66e47cc52cde0b9e9f4a
notsogoodaditya <79082526+notsogoodaditya@users.noreply.github.com>
2022-09-29 17:59:21 +0530

    Time: 86 ms (89.22%), Space: 67.3 MB (95.81%) - LeetHub

--- 2121-intervals-between-identical-elements/2121-intervals-between-identical-elements.java
+++ 2121-intervals-between-identical-elements/2121-intervals-between-identical-elements.java
@@ -1,20 +1,20 @@
 class Solution {
     long[] ans;
-    List<Integer> list;
+    List<Long> list;
     // HashMap<Integer,Long> indx_sum = new HashMap<>();
     long[] indx_sum = new long[1_0_0_0_0_1];
     public long[] getDistances(int[] arr) {
         int n = arr.length;
         this.ans = new long[n];
-        HashMap<Integer,List<Integer>> map = new HashMap<>();
+        HashMap<Integer,List<Long>> map = new HashMap<>();
         for(int i=0;i<n;i++){
            if(map.get(arr[i])==null){
                list = new ArrayList<>();
-               list.add(i);
+               list.add((long)i);
                map.put(arr[i],list);
            }else{
                list = map.get(arr[i]);
-               list.add(i);
+               list.add((long)i);
                map.put(arr[i],list);
            }
             indx_sum[arr[i]] += i;
@@ -28,14 +28,15 @@ class Solution {
         // solve(3,map.get(3));
         return ans;
     }
-    public void solve(int x,List<Integer> list){
+    public void solve(int x,List<Long> list){
         long sum = indx_sum[x];
         long pre = 0l;
         int n = list.size();
         for(int i=0;i<n;i++){
-            ans[list.get(i)] = (long)sum-((long)list.get(i)*((long)(n-i)))+Math.abs(pre-(long)((long)list.get(i)*(long)i));
-            pre += (long)list.get(i);
-            sum -= (long)list.get(i);
+            int indx = list.get(i).intValue();
+            ans[indx] = sum-(list.get(i)*((long)(n-i)))+Math.abs(pre-(long)(list.get(i)*(long)i));
+            pre += list.get(i);
+            sum -= list.get(i);
             // System.out.println(sum+" "+pre);
         }
     }
